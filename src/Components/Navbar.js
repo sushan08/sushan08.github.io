8e32d8f17cc7679598a852916b1a60038229e697
Sushan Shrestha <onilxtha00006@gmail.com>
2025-06-03 18:47:21 +0545

    update navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -42,7 +42,7 @@ const Nav = () => {
   return (
     <nav className={navbarblur? 'Navbar blur':'Navbar'}> 
  
-        <h1 title='Reload' onClick={()=>window.location.reload(true)} className='Logo'>SS</h1>
+        <h1 title='Reload' onClick={()=>window.location.reload(true)} className='Logo'>&lt;/SUS&gt;</h1>
 
         <div className='Hamburger' onClick={showMenu}>
             <span className='bar'></span>
